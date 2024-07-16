3097c9c3e0bd54eae07df1145b235a97c4a25943
Bimgor_Ayk <haykooo3@gmail.com>
2024-07-16 13:36:50 +0300

    ofisse

--- main.py
+++ main.py
@@ -19,51 +19,36 @@ class App(ctk.CTk):
         self.grid_columnconfigure(1, weight=1)
 
         # load images with light and dark mode image
-        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
-        self.logo_image = ctk.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")),
-                                       size=(26, 26))
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+        self.logo_image = ctk.CTkImage(Image.open(os.path.join(path, "logo_single.png")), size=(26, 26))
 
-        self.home_image = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
-                                       dark_image=Image.open(os.path.join(image_path, "home_light.png")),
-                                       size=(20, 20))
-        self.chat_image = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "chat_dark.png")),
-                                       dark_image=Image.open(os.path.join(image_path, "chat_light.png")),
-                                       size=(20, 20))
-        self.add_user_image = ctk.CTkImage(
-            light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
-            dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
+        self.home_image = ctk.CTkImage(Image.open(os.path.join(path, "home_light.png")), size=(20, 20))
+        self.chat_image = ctk.CTkImage(Image.open(os.path.join(path, "chat_light.png")), size=(20, 20))
+        self.add_user_image = ctk.CTkImage(Image.open(os.path.join(path, "add_user_light.png")), size=(20, 20))
 
         # create navigation frame
         self.navigation_frame = ctk.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        self.navigation_frame_label = ctk.CTkLabel(self.navigation_frame, text="  Image Example",
-                                                   image=self.logo_image,
-                                                   compound="left",
-                                                   font=ctk.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label = ctk.CTkLabel(self.navigation_frame, text="  Image Example", image=self.logo_image,
+                                                   compound="left", font=("Roboto", 16))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
         self.home_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10,
-                                         text="Home",
-                                         fg_color="transparent", text_color=("gray10", "gray90"),
-                                         hover_color=("gray70", "gray30"),
+                                         text="Home", fg_color="transparent", text_color="gray90", hover_color="gray30",
                                          image=self.home_image, anchor="w", command=self.home_button_event)
         self.home_button.grid(row=1, column=0, sticky="ew")
 
-        self.frame_2_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40,
-                                            border_spacing=10, text="Frame 2",
-                                            fg_color="transparent", text_color=("gray10", "gray90"),
-                                            hover_color=("gray70", "gray30"),
-                                            image=self.chat_image, anchor="w",
+        self.frame_2_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10,
+                                            text="Frame 2", fg_color="transparent", text_color="gray90",
+                                            hover_color="gray30", image=self.chat_image, anchor="w",
                                             command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40,
-                                            border_spacing=10, text="Frame 3",
-                                            fg_color="transparent", text_color=("gray10", "gray90"),
-                                            hover_color=("gray70", "gray30"),
-                                            image=self.add_user_image, anchor="w",
+        self.frame_3_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40,border_spacing=10,
+                                            text="Frame 3", fg_color="transparent", text_color="gray90",
+                                            hover_color="gray30", image=self.add_user_image, anchor="w",
                                             command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
 
